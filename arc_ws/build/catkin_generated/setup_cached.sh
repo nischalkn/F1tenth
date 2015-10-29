76b1b640f44e1064b562c76b5e788021e961b2d1
Nischal <nischalkn@gmail.com>
2015-10-28 20:04:53 -0400

    Catkin workspace created

---
+++ arc_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/nischal/Projects/arc/arc_ws/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/nischal/Projects/arc/arc_ws/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/home/nischal/Projects/arc/arc_ws/devel/lib:/home/nischal/Projects/arc/arc_ws/devel/lib/x86_64-linux-gnu:/opt/ros/indigo/lib/x86_64-linux-gnu:/usr/local/cuda-6.5/lib64:/usr/local/cuda/lib64/:/opt/ros/indigo/lib"
+export PATH="/home/nischal/Projects/arc/arc_ws/devel/bin:/opt/ros/indigo/bin:/usr/local/cuda/bin/:/home/nischal/android-studio/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/usr/local/cuda-6.5/bin:/usr/local/cuda-6.5/bin"
+export PKG_CONFIG_PATH="/home/nischal/Projects/arc/arc_ws/devel/lib/pkgconfig:/home/nischal/Projects/arc/arc_ws/devel/lib/x86_64-linux-gnu/pkgconfig:/opt/ros/indigo/lib/x86_64-linux-gnu/pkgconfig:/opt/ros/indigo/lib/pkgconfig"
+export PWD="/home/nischal/Projects/arc/arc_ws/build"
+export PYTHONPATH="/home/nischal/Projects/arc/arc_ws/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/nischal/Projects/arc/arc_ws/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/nischal/Projects/arc/arc_ws/src:$ROS_PACKAGE_PATH"
---
+++ arc_ws/build/race/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/nischal/Projects/arc/arc_ws/src/race
+# Build directory: /home/nischal/Projects/arc/arc_ws/build/race
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
