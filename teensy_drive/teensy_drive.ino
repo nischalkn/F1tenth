f4e46155b0651756560a1dc7efb4900dbd5cdb81
pariljain <paril@seas.upenn.edu>
2015-10-28 21:55:53 -0400

    Minor changes in teensy

--- teensy_drive/teensy_drive.ino
+++ teensy_drive/teensy_drive.ino
@@ -6,15 +6,46 @@ ros::NodeHandle  nh;
 
 
 boolean flagStop = false;
-int pwm_center_value = 9830;
+int pwm_center_value = 9830;  //  15% duty cycle
+int pwm_lowerlimit = 6554;    //  10% duty cycle
+int pwm_upperlimit = 13108;   //  20% duty cycle
 
 void messageDrive( const race::drive_values& pwm )
 {
-
+  Serial.print("Pwm drive : ");
+  Serial.println(pwm.pwm_drive);
+  Serial.print("Pwm angle : ");
+  Serial.println(pwm.pwm_angle);
+  
   if(flagStop == false)
   {
-    analogWrite(5,pwm.pwm_drive);  
-    analogWrite(6,pwm.pwm_angle);     
+    if(pwm.pwm_drive < pwm_lowerlimit)
+    {
+      analogWrite(5,pwm_lowerlimit);    //  Safety lower limit        
+    }
+    else if(pwm.pwm_drive > pwm_upperlimit)
+    {
+      analogWrite(5,pwm_upperlimit);    //  Safety upper limit
+    }
+    else
+    {
+      analogWrite(5,pwm.pwm_drive);     //  Incoming data                    
+    }
+
+    
+    if(pwm.pwm_angle < pwm_lowerlimit)
+    {
+      analogWrite(6,pwm_lowerlimit);    //  Safety lower limit        
+    }
+    else if(pwm.pwm_angle > pwm_upperlimit)
+    {
+      analogWrite(6,pwm_upperlimit);    //  Safety upper limit
+    }
+    else
+    {
+      analogWrite(6,pwm.pwm_angle);     //  Incoming data                    
+    }
+
   }
   else
   {
@@ -26,9 +57,13 @@ void messageDrive( const race::drive_values& pwm )
 void messageEmergencyStop( const std_msgs::Bool& flag )
 {
   flagStop = flag.data;
-  analogWrite(5,pwm_center_value);
-  analogWrite(6,pwm_center_value);    
+  if(flagStop == true)
+  {
+    analogWrite(5,pwm_center_value);
+    analogWrite(6,pwm_center_value);    
+  }
 }
+
 ros::Subscriber<race::drive_values> sub_drive("drive_pwm", messageDrive );
 ros::Subscriber<std_msgs::Bool> sub_stop("eStop", messageEmergencyStop );
 
@@ -42,6 +77,8 @@ void setup() {
   analogWrite(6,pwm_center_value);
   pinMode(13,OUTPUT); 
   digitalWrite(13,HIGH);
+  pinMode(2,OUTPUT); 
+  digitalWrite(2,HIGH);
 
   nh.initNode();
   nh.subscribe(sub_drive);
@@ -50,6 +87,8 @@ void setup() {
 
 void loop() {
   nh.spinOnce();
+  
+  
   // put your main code here, to run repeatedly:
   /*
   if(Serial.available())
